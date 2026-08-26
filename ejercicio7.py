07e7a4f82493691c26adff89f73de000d90062f6
santiagoochoa-rgb <santijimenez277@gmail.com>
2026-08-26 17:06:54 -0500

    ejercicio7.py

---
+++ ejercicio7.py
@@ -0,0 +1,10 @@
+producto = input("Ingrese el nombre del producto: ")
+precio = float(input("Ingrese el precio: "))
+cantidad = int(input("Ingrese la cantidad: "))
+
+total = precio * cantidad
+
+print("Producto:", producto)
+print("Precio unitario:", precio)
+print("Cantidad:", cantidad)
+print("Total:", total)
