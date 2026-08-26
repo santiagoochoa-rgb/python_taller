7fa065e8926f4755543f7147252daf0976c68504
santiagoochoa-rgb <santijimenez277@gmail.com>
2026-08-26 17:01:26 -0500

    ejercicio4.py

---
+++ ejercicio4.py
@@ -0,0 +1,7 @@
+a = float(input("Ingrese el primer número: "))
+b = float(input("Ingrese el segundo número: "))
+
+print("Suma:", a + b)
+print("Resta:", a - b)
+print("Multiplicacion:", a * b)
+print("División, a / b)
