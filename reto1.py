d77c4df58b84b348ea7e9970c7bc0d8495f37788
santiagoochoa-rgb <santijimenez277@gmail.com>
2026-08-26 17:16:07 -0500

    reto1.py

---
+++ reto1.py
@@ -0,0 +1,10 @@
+nombre = input("Nombre del empleado: "))
+horas = int("Horas trabajadas"))
+valor_horas = float(input("Valor de cada hora: "))
+
+salario = horas * valor_hora
+
+print("Empleado:", nombre)
+print("Horas trabajadas:", horas)
+print("Valor hora:", valor_hora)
+print("Salario:", salario)
