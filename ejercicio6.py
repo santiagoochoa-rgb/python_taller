253cd6dd99118ea1656dd649636ecc9c3f0dead8
santiagoochoa-rgb <santijimenez277@gmail.com>
2026-08-26 17:04:40 -0500

    ejercicio6.py

---
+++ ejercicio6.py
@@ -0,0 +1,7 @@
+nombre = input("Ingrese su nombre: ")
+nacimiento = int(input("Ingrese su año de nacimiento: "))
+actual = int(input("Ingrese el año actual: "))
+
+edad = actual - nacimiento
+
+print(nombre, "tiene aproimadamente", edad, "años")
