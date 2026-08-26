78f94f1ce0972dad3f6bb962689bf79063151b97
santiagoochoa-rgb <santijimenez277@gmail.com>
2026-08-26 17:27:04 -0500

    actividad_de_analisis.py
    1. ¿Qué problema presenta?
    Da un error porque edad es un texto y se intenta sumarle el número 5.
    
    2. ¿Qué tipo de dato devuelve input()?
    Devuelve un str, es decir, texto.
    
    3. ¿Cómo se puede corregir?
    Usando int() para convertir la edad en un número entero.

---
+++ actividad_de_analisis.py
@@ -0,0 +1,13 @@
+nombre = input("Ingrese su nombre: ")
+edad = input("Ingrese su edad: ")
+nueva_edad = edad + 5
+
+
+#codigo corregido
+nombre = input("Ingrese su nombre: ")
+edad = int(input("Ingrese su edad: "))
+
+nueva_edad = edad + 5
+
+print(nombre)
+prnt(nueva_edad)
