1cb04e0a977ab04204cf3d551a37182eef5ebc4a
santiagoochoa-rgb <santijimenez277@gmail.com>
2026-08-26 17:19:05 -0500

    reto3.py

---
+++ reto3.py
@@ -0,0 +1,8 @@
+nombre = input("Ingrese su nombre: ")
+peso = float(input("Ingrese su peso: "))
+estatura = float(input("Ingrese su estatura: "))
+
+imc = peso / (estatura * estatura)
+
+print("Nombre:", nombre)
+print("IMC:", imc)
