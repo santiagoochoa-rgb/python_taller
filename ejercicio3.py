a637cd6e79ff72a6c3e4f41b1112347386a828cb
santiagoochoa-rgb <santijimenez277@gmail.com>
2026-08-26 16:58:50 -0500

    ejercicio3.py

---
+++ ejercicio3.py
@@ -0,0 +1,9 @@
+nombre = input("Ingrese su nombre: ")
+edad = input("Ingrese su edad: ")
+carrera = input("Ingrese su carrera: ")
+semestre = input("Ingrese su semestre: ")
+
+print("Hola", nombre)
+print("Tienes", edad, "años")
+print("Estudias", carrera)
+print("Estás en", semestre, "semestre")
