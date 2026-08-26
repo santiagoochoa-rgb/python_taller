1129e6f5eb4301416dbd9bb8ec8e860d642e5ce6
santiagoochoa-rgb <santijimenez277@gmail.com>
2026-08-26 17:09:24 -0500

    ejercicio8.py

---
+++ ejercicio8.py
@@ -0,0 +1,10 @@
+nota1 = float(input("Ingrese la nota 1: "))
+nota2 = float(input("Ingrese la nota 2: "))
+nota3 = float(input("Ingrese la nota 3: "))
+
+promedio = (nota1 + nota2 + nota3) / 3
+
+print("Nota 1:", nota1)
+print("Nota 2:", nota2)
+print("Nota 3:", nota3)
+print("Promedio:", promedio)
