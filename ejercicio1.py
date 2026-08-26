e367362d235e0f105e1aaef9d3c097cde16c0d5f
santiagoochoa-rgb <santijimenez277@gmail.com>
2026-08-26 16:54:23 -0500

    Create ejercicio1.py

---
+++ ejercicio1.py
@@ -0,0 +1,11 @@
+nombre = "Santiago Ochoa"
+edad = 17
+semestre = 2
+programa = "Ingeneria de sistemas"
+promedio = 4.0
+
+print("nombre:", nombre)
+print("edad:", edad)
+print("semestre:", semestre)
+print("programa:", programa)
+print("promedio:", promedio)
